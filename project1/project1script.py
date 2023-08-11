9877ed9a7a75079811367aa3653b0bf823370c9d
JuliRecio <julian.recio@ing.austral.edu.ar>
2023-08-11 00:25:59 -0300

    Proyecto 1: - Se abre la cámara para capturar video - Se convierte el último frame a gris - Se le aplica un threshhold con barra de desplazamiento

--- project1/project1script.py
+++ project1/project1script.py
@@ -1,2 +1,35 @@
-import cv2
+import imutils
+import cv2 as cv
+
+video = cv.VideoCapture(0)
+
+while (True) :
+    ret, frame = video.read()
+
+    cv.imshow('frame', frame)
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+grayFrame = cv.cvtColor(frame,cv.COLOR_BGRA2GRAY)
+cv.imshow('grayFrame', grayFrame)
+cv.waitKey(0)
+
+ret1, thresh1 = cv.threshold(grayFrame, 0, 255, cv.THRESH_BINARY)
+cv.imshow("Binary", thresh1)
+
+def binary(val):
+    ret1, thresh1 = cv.threshold(grayFrame, val, 255, cv.THRESH_BINARY)
+    cv.imshow("Binary", thresh1)
+
+
+cv.namedWindow('Binary')
+cv.createTrackbar('Trackbar', 'Binary', 0, 255, binary)
+
+binary(0)
+
+cv.waitKey()
+
+print("fin")
 
