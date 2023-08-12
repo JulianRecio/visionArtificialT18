e325a6c32c0bd8f333a0e3dcbc2aa798dcce68af
JuliRecio <julian.recio@ing.austral.edu.ar>
2023-08-12 12:01:12 -0300

    Proyecto 1: - Se aplica tanto erosion como dilatacion

--- project1/project1script.py
+++ project1/project1script.py
@@ -1,4 +1,5 @@
 import imutils
+import numpy as np
 import cv2 as cv
 
 video = cv.VideoCapture(0)
@@ -23,13 +24,25 @@ def binary(val):
     ret1, thresh1 = cv.threshold(grayFrame, val, 255, cv.THRESH_BINARY)
     cv.imshow("Binary", thresh1)
 
+binary(100)
 
 cv.namedWindow('Binary')
 cv.createTrackbar('Trackbar', 'Binary', 0, 255, binary)
 
-binary(0)
-
 cv.waitKey()
 
+#erosion
+
+kernel = np.ones((3,3), np.uint8)
+erosion = cv.morphologyEx(thresh1, cv.MORPH_ERODE, kernel)
+cv.imshow("erosion",erosion)
+
+#close
+
+dilation = cv.morphologyEx(erosion, cv.MORPH_DILATE, kernel)
+cv.imshow("dilation",dilation)
+
+
+cv.waitKey(0)
 print("fin")
 
